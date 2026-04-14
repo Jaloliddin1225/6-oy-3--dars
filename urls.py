f203f73c6a69b4f5b7125227d82fe8f2da90db3f
Jaloliddin <uikjek@gmail.com>
2026-04-14 16:36:54 +0500

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import home, book_detail
-
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('books/<int:book_id>/', book_detail, name='detail'),
-]
